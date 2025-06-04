dae24d93fd945bc81718156d0c9fc79d0eb630f9
raulonastool <102239225+Raulonastool@users.noreply.github.com>
2025-06-04 13:06:03 -0500

    Add basic Hardhat tests

--- test/HitboxGame.test.js
+++ test/HitboxGame.test.js
@@ -1,1 +1,39 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
+describe("HitboxGame", function () {
+  let game;
+
+  beforeEach(async function () {
+    const HitboxGame = await ethers.getContractFactory("HitboxGame");
+    game = await HitboxGame.deploy();
+    await game.waitForDeployment();
+  });
+
+  it("starts at position (0,0)", async function () {
+    const pos = await game.character();
+    expect(pos.x).to.equal(0n);
+    expect(pos.y).to.equal(0n);
+  });
+
+  it("moves the character", async function () {
+    await (await game.move(1, 2)).wait();
+    const pos = await game.character();
+    expect(pos.x).to.equal(1n);
+    expect(pos.y).to.equal(2n);
+  });
+
+  it("does not move below zero", async function () {
+    await (await game.move(1, 2)).wait();
+    await (await game.move(-5, -3)).wait();
+    const pos = await game.character();
+    expect(pos.x).to.equal(0n);
+    expect(pos.y).to.equal(0n);
+  });
+
+  it("stores revealed tiles", async function () {
+    await (await game.reveal(5, 6)).wait();
+    const key = ethers.solidityPackedKeccak256(["uint256", "uint256"], [5, 6]);
+    expect(await game.revealed(key)).to.equal(true);
+  });
+});
--- test/HitboxNFT.test.js
+++ test/HitboxNFT.test.js
@@ -1,1 +1,35 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
+describe("HitboxNFT", function () {
+  let game, nft, owner, other;
+
+  beforeEach(async function () {
+    [owner, other] = await ethers.getSigners();
+    const HitboxGame = await ethers.getContractFactory("HitboxGame");
+    game = await HitboxGame.deploy();
+    await game.waitForDeployment();
+    const HitboxNFT = await ethers.getContractFactory("HitboxNFT");
+    nft = await HitboxNFT.deploy(await game.getAddress());
+    await nft.waitForDeployment();
+  });
+
+  it("mints an NFT to the owner", async function () {
+    await (await nft.mint(owner.address)).wait();
+    expect(await nft.ownerOf(1n)).to.equal(owner.address);
+    expect(await nft.nextTokenId()).to.equal(2n);
+  });
+
+  it("mints to another address", async function () {
+    await (await nft.mint(other.address)).wait();
+    expect(await nft.ownerOf(1n)).to.equal(other.address);
+  });
+
+  it("allows multiple mints", async function () {
+    await (await nft.mint(owner.address)).wait();
+    await (await nft.mint(owner.address)).wait();
+    expect(await nft.ownerOf(1n)).to.equal(owner.address);
+    expect(await nft.ownerOf(2n)).to.equal(owner.address);
+    expect(await nft.nextTokenId()).to.equal(3n);
+  });
+});
