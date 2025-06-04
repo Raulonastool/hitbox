a4251859b6a1f22f66b34a1d80ae7f9eaa5f4381
raulonastool <102239225+Raulonastool@users.noreply.github.com>
2025-06-04 12:24:47 -0500

    Add Hardhat config, contracts, and scripts

--- contracts/HitboxNFT.sol
+++ contracts/HitboxNFT.sol
@@ -1,1 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract HitboxNFT is ERC721 {
+    address public game;
+    uint256 public nextTokenId = 1;
+
+    constructor(address _game) ERC721("HitboxNFT", "HIT") {
+        game = _game;
+    }
+
+    function mint(address to) external {
+        _safeMint(to, nextTokenId);
+        nextTokenId++;
+    }
+}
